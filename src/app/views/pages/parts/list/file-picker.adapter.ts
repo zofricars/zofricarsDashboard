833ee6e4753a3c87db9fc710c8281458544c59c5
zofricars <123201522+zofricars@users.noreply.github.com>
2023-10-04 13:47:44 -0500

    urlimg file-picker.adapter.ts

--- src/app/views/pages/parts/list/file-picker.adapter.ts
+++ src/app/views/pages/parts/list/file-picker.adapter.ts
@@ -16,13 +16,13 @@ export class DemoFilePickerAdapter extends FilePickerAdapter {
     
     const form = new FormData();
     form.append('file', fileItem.file);
-    const api = 'https://db.buckapi.com:3333/api/containers/tixsImages/upload';
+    const api = 'https://db.buckapi.com:3333/imgApi/containers/tixsImages/upload';
     const req = new HttpRequest('POST', api, form, {reportProgress: true});
     return this.http.request(req)
     .pipe(
       map( (res: HttpEvent<any>) => {
           if (res.type === HttpEventType.Response) {
-          this._butler.partImages.push('https://db.buckapi.com/api/server/local-storage/tixsImages/'+res.body.result.files.file[0].name);
+          this._butler.partImages.push('https://db.buckapi.com/imgApi/server/local-storage/tixsImages/'+res.body.result.files.file[0].name);
           this._butler.newPartImage=true;
           return res.body.id.toString();
         } else if (res.type ===  HttpEventType.UploadProgress ) {
