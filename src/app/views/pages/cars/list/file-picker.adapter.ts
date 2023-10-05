75e6fac80af93f36ad3f9785c33b9ca33a44aa28
zofricars <123201522+zofricars@users.noreply.github.com>
2023-10-04 19:45:28 -0500

    www file-picker.adapter.ts

--- src/app/views/pages/cars/list/file-picker.adapter.ts
+++ src/app/views/pages/cars/list/file-picker.adapter.ts
@@ -22,7 +22,7 @@ export class DemoFilePickerAdapter extends FilePickerAdapter {
     .pipe(
       map( (res: HttpEvent<any>) => {
           if (res.type === HttpEventType.Response) {
-          this._butler.partImages.push('https://db.buckapi.com/api/server/local-storage/cars/'+res.body.result.files.file[0].name);
+          this._butler.partImages.push('https://www.buckapi.com/api/server/local-storage/cars/'+res.body.result.files.file[0].name);
           this._butler.newPartImage=true;
           return res.body.id.toString();
         } else if (res.type ===  HttpEventType.UploadProgress && res.total  !== undefined) {
